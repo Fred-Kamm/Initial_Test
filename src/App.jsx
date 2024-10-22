6c961cb9160879a00fc6324e6e269e7bf675b03a
Fred Kamm <fred.kamm@proactiveusa.com>
2024-10-22 09:34:38 -0700

    staging updated files

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Doing an update on the new cpu</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
